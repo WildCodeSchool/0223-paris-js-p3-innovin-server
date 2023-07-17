2ece8283975d8799962ef538e14d3bed5af30ffc
T0MLE <lelaurain.tom@gmail.com>
2023-07-17 10:58:03 +0200

    updated sql request to also fetch wine color

--- src/modules/note/model.js
+++ src/modules/note/model.js
@@ -3,7 +3,7 @@ const db = require("../../config/db-config");
 const findNotesByUserAndSessionId = (sessionId, userId) => {
   return db
     .execute(
-      "SELECT tag.name as tag, wine.name as wine, note, wine_id from note_has_tag JOIN tag on tag.id = note_has_tag.tag_id JOIN note ON note.id = note_has_tag.note_id JOIN wine ON wine.id = note.wine_id WHERE session_id = ? and user_id = ? ORDER BY note DESC;",
+      "SELECT tag.name as tag, category, wine.name as wine, color, note, wine_id from note_has_tag JOIN tag on tag.id = note_has_tag.tag_id JOIN note ON note.id = note_has_tag.note_id JOIN wine ON wine.id = note.wine_id WHERE session_id = ? and user_id = ? ORDER BY note DESC",
       [sessionId, userId]
     )
     .then((data) => {
@@ -12,13 +12,13 @@ const findNotesByUserAndSessionId = (sessionId, userId) => {
 };
 
 const AddOne = (userId, sessionId) => {
-    return db.execute(`insert recipe (user_id, session_id) values (?, ?)`, [
-      userId,
-      sessionId,
-    ]);
-  };
+  return db.execute(`insert recipe (user_id, session_id) values (?, ?)`, [
+    userId,
+    sessionId,
+  ]);
+};
 
 module.exports = {
-    findNotesByUserAndSessionId,
-    AddOne
+  findNotesByUserAndSessionId,
+  AddOne,
 };
