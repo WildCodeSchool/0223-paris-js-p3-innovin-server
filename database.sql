a7a6fc2159b8bdfb823b4f75c2130e7858b426b6
ThomasLonjon <lonjon.thomas@gmail.com>
2023-07-17 13:59:52 +0200

    added regions and cepage to DB

--- database.sql
+++ database.sql
@@ -27,12 +27,15 @@ CREATE TABLE IF NOT EXISTS `mydb`.`wine` (
   `color` VARCHAR(45) NOT NULL,
   `manufacture_year` VARCHAR(45) NOT NULL,
   `domain` VARCHAR(100) NOT NULL,
-  `region` VARCHAR(100) NOT NULL,
+  `region_id` INT NOT NULL,
   `appellation` VARCHAR(100) NOT NULL,
   `cepage` VARCHAR(255) NOT NULL,
   `image` VARCHAR(255) NOT NULL,
   `comment` TEXT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`),
+   CONSTRAINT `fk_region_1`
+    FOREIGN KEY (`region_id`)
+    REFERENCES `mydb`.`region` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
@@ -247,3 +250,42 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
+-- -----------------------------------------------------
+-- Table `mydb`.`region`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`region` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `description` TEXT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cepage`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`cepage` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `color` enum('red','white') NOT NULL,
+  `name` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`region_has_cepage`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`region_has_cepage` (
+  `region_id` INT NOT NULL,
+  `cepage_id` INT NOT NULL,
+  PRIMARY KEY (`region_id`, `cepage_id`),
+  CONSTRAINT `fk_region_has_cepage_region1`
+    FOREIGN KEY (`region_id`)
+    REFERENCES `mydb`.`region` (`id`),
+  CONSTRAINT `fk_region_has_cepage_cepage1`
+    FOREIGN KEY (`cepage_id`)
+    REFERENCES `mydb`.`cepage` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
